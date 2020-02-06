b114c613fd59cf97c972fc39cbadf3b004ba6c9d
sudeep16 <srshakya5@gmail.com>
2020-02-06 16:18:28 +0545

    Malfunction in Registration..
    Registration Complete

--- app/src/main/java/com/user/songratingsystem/bll/LoginBll.java
+++ app/src/main/java/com/user/songratingsystem/bll/LoginBll.java
@@ -1,27 +1,36 @@
 package com.user.songratingsystem.bll;
 
+import com.user.songratingsystem.api.UsersAPI;
+import com.user.songratingsystem.responses.RegisterResponse;
+import com.user.songratingsystem.url.URL;
+
+import java.io.IOException;
+
+import retrofit2.Call;
+import retrofit2.Response;
+
 public class LoginBll {
-//    boolean isSuccess = false;
-//
-//    public boolean checkUser(String Username, String Password) {
-//
-//        UsersAPI usersAPI = URL.getInstance().create(UsersAPI.class);
-//        Call<RegisterResponse> usersCall = usersAPI.checkUser(Username, Password);
-//
-//        try {
-//            Response<RegisterResponse> loginResponse = usersCall.execute();
-//            if (loginResponse.isSuccessful() &&
-//                    loginResponse.body().getStatus().equals("Login success!")) {
-//
-//                URL.token += loginResponse.body().getUsertoken();
-//                // Url.Cookie = imageResponseResponse.headers().get("Set-Cookie");
-//                isSuccess = true;
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        return isSuccess;
-//    }
+    boolean isSuccess = false;
+
+    public boolean checkUser(String Username, String Password) {
+
+        UsersAPI usersAPI = URL.getInstance().create(UsersAPI.class);
+        Call<RegisterResponse> usersCall = usersAPI.checkUser(Username, Password);
+
+        try {
+            Response<RegisterResponse> loginResponse = usersCall.execute();
+            if (loginResponse.isSuccessful() &&
+                    loginResponse.body().getStatus().equals("Login success!")) {
+
+                URL.token += loginResponse.body().getUsertoken();
+                // Url.Cookie = imageResponseResponse.headers().get("Set-Cookie");
+                isSuccess = true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return isSuccess;
+    }
 }
 
 
