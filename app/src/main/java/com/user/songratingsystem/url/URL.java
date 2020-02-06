a0ff6d4d5e7f35ab503b7b3af3f38e19e86eb4fd
sudeep16 <srshakya5@gmail.com>
2020-02-06 20:05:00 +0545

    Music

--- app/src/main/java/com/user/songratingsystem/url/URL.java
+++ app/src/main/java/com/user/songratingsystem/url/URL.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class URL {
-        // public static final String base_url = "http://192.168.1.11:2020/";
+//         public static final String base_url = "http://192.168.1.9:2020/";
         public static final String base_url = "http://10.0.2.2:2020/";
         // public static final String base_url = "http://172.100.100.5:2020/";
 
