0ac90f14969a31ba5f2f10bb0c486cfcbc1e7b8e
sudeep16 <srshakya5@gmail.com>
2020-02-11 16:17:47 +0545

    About Us

--- app/src/main/java/com/user/songratingsystem/ui/aboutus/AboutusFragment.java
+++ app/src/main/java/com/user/songratingsystem/ui/aboutus/AboutusFragment.java
@@ -4,20 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
 import android.widget.TextView;
 
-import androidx.annotation.Nullable;
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.user.songratingsystem.R;
 
-public class AboutusFragment extends Fragment {
+public class AboutusFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap mapView;
+    TextView detail;
+
+
     public AboutusFragment() {
         // Required empty public constructor
     }
@@ -27,20 +32,29 @@ public class AboutusFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_aboutus, container, false);
+        View view = inflater.inflate(R.layout.fragment_aboutus, container, false);
 
-        WebView myWebView = view.findViewById(R.id.webview);
-        myWebView.loadUrl("https://softwarica.edu.np");
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
 
-        //enable javascript setting
-        WebSettings webSettings = myWebView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setAppCacheEnabled(true);
-
-        myWebView.setWebViewClient(new WebViewClient());
+        detail = view.findViewById(R.id.detail);
+        detail.setText("Song Rating System is an application which tries to focus in songs that are liked by users and tries to recommend them about the latest songs and try to receive their porsitive response.");
 
 
         return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mapView = googleMap;
+
+        CameraUpdate center, zoom;
+        center = CameraUpdateFactory.newLatLng(new LatLng(27.7177913, 85.2968171));
+        zoom = CameraUpdateFactory.zoomTo(10);
+
+        mapView.addMarker(new MarkerOptions().position(new LatLng(27.7177913, 85.2968171)).title("Song Rating Server"));
+        mapView.moveCamera(center);
+        mapView.animateCamera(zoom);
+        mapView.getUiSettings().setZoomControlsEnabled(true);
+    }
 }
